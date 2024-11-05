01a219dbdf2ee23bc2ae80d1fa11e5df6fc46807
phamanhduc <zyzz15620@gmail.com>
2024-11-05 20:54:05 +0700

    [ID-3] Improve ButtonTests

--- src/test/java/testcases/elements/ButtonTests.java
+++ src/test/java/testcases/elements/ButtonTests.java
@@ -2,11 +2,12 @@ package testcases.elements;
 
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ButtonTests extends BaseTest {
+class ButtonTests extends BaseTest {
     Locator pageHeader;
     Locator clickedButton;
     Locator buttons;
@@ -15,33 +16,44 @@ public class ButtonTests extends BaseTest {
     void createContextAndPage() {
         context = browser.newContext();
         page = context.newPage();
+        page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/elements/button");
 
         pageHeader = page.locator("//span[contains(concat(' ', normalize-space(@class), ' '), 'ant-divider-inner-text')][contains(text(), 'Common button type')]");
-        clickedButton = page.locator("//div[contains(text()[2], 'was clicked!')]//span[contains(concat(' ', normalize-space(@class), ' '), ' text-rose-500 ')]");
+        clickedButton = page.locator("//div[contains(., 'Button') and contains(., 'was clicked!') and ./span[contains(concat(' ', normalize-space(@class), ' '), ' text-rose-500 ')]]");
         buttons = page.locator("//div[contains(concat(' ',normalize-space(@class),' '),' ant-flex css-vryruh ant-flex-wrap-wrap ant-flex-align-center ant-flex-gap-small ')]/*");
     }
 
     @Test
     void verifyNavigateSuccess() {
-        page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/elements/button");
         assertThat(page).hasTitle("Test With Me aka Tho Test");
         assertThat(pageHeader).isVisible();
     }
 
     @Test
-    void verifyButtonFunction(){
-        page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/elements/button");
-
+    void verifyButtonFunction(){ //vấn đề của cách này là fail một cái thì nó break hết nên là chưa hay
         for(int i = 0; i < buttons.count(); i++){
             buttons.nth(i).click();
             String buttonValue = buttons.nth(i).getAttribute("value");
             String buttonText = buttons.nth(i).innerText();
 
             if(buttonValue != null){
-                assertEquals(buttonValue, clickedButton.innerText());
+                assertThat(clickedButton).hasText(buttonValue);
             } else {
-                assertEquals(buttonText, clickedButton.innerText());
+                assertThat(clickedButton).hasText(buttonText);
             }
         }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Div button", "Origin button", "Input button", "Default", "Primary", "Dashed", "Text", "Link", "Icon button"})
+    void shouldClickButton(String buttonLabelData){
+        String buttonLabel = buttonLabelData;
+        String normalButtonXpath = String.format("//button[normalize-space(.//text())='%s']", buttonLabel);
+        String tagButtonXpath = String.format("//*[@role='button' and normalize-space(.//text())='%s']", buttonLabel);
+        String typeButtonXpath = String.format("//input[@type='button' and normalize-space(.//@value)='%s']", buttonLabel);
+        String buttonXpath = String.format("%s | %s | %s", normalButtonXpath, tagButtonXpath, typeButtonXpath);
+        Locator buttonLocator = page.locator(buttonXpath);
+        buttonLocator.click();
+        assertThat(clickedButton).hasText(String.format("Button %s was clicked!", buttonLabel));
     }
 }
--- src/test/java/testcases/elements/CheckboxTests.java
+++ src/test/java/testcases/elements/CheckboxTests.java
@@ -7,42 +7,73 @@ import org.junit.jupiter.api.Test;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CheckboxTests extends BaseTest {
+class CheckboxTests extends BaseTest {
     Locator pageHeader;
-    Locator clickedButton;
-    Locator buttons;
+    Locator checkedCheckboxText;
+    Locator allCheckbox;
+    Locator allCheckboxText;
 
     @BeforeEach
     void createContextAndPage() {
         context = browser.newContext();
         page = context.newPage();
+        page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/elements/checkbox");
 
-        pageHeader = page.locator("//span[contains(concat(' ', normalize-space(@class), ' '), 'ant-divider-inner-text')][contains(text(), 'Common button type')]");
-        clickedButton = page.locator("//div[contains(text()[2], 'was clicked!')]//span[contains(concat(' ', normalize-space(@class), ' '), ' text-rose-500 ')]");
-        buttons = page.locator("//div[contains(concat(' ',normalize-space(@class),' '),' ant-flex css-vryruh ant-flex-wrap-wrap ant-flex-align-center ant-flex-gap-small ')]/*");
+        pageHeader = page.locator("//span[contains(concat(' ', normalize-space(@class), ' '), 'ant-divider-inner-text')][contains(text(), 'Checkbox')]");
+        checkedCheckboxText = page.locator("//div[contains(text(), 'Selected values:')]//span[contains(concat(' ',normalize-space(@class),' '),' text-rose-500 ')]");
+        allCheckbox = page.locator("//div[contains(concat(' ',normalize-space(@class),' '),' ant-checkbox-group css-vryruh ')]/*//input[@type='checkbox']");
+        allCheckboxText = page.locator("//div[contains(concat(' ',normalize-space(@class),' '),' ant-checkbox-group css-vryruh ')]/*//span[text()]");
+
     }
 
     @Test
     void verifyNavigateSuccess() {
-        page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/elements/button");
         assertThat(page).hasTitle("Test With Me aka Tho Test");
         assertThat(pageHeader).isVisible();
     }
 
     @Test
-    void verifyButtonFunction(){
-        page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/elements/button");
-
-        for(int i = 0; i < buttons.count(); i++){
-            buttons.nth(i).click();
-            String buttonValue = buttons.nth(i).getAttribute("value");
-            String buttonText = buttons.nth(i).innerText();
-
-            if(buttonValue != null){
-                assertEquals(buttonValue, clickedButton.innerText());
+    void verifyFirstIsCheckedByDefault(){
+        for(int i = 0; i < allCheckbox.count(); i++ ){
+            if(i == 0) {
+                assertThat(allCheckbox.nth(0)).isChecked();
             } else {
-                assertEquals(buttonText, clickedButton.innerText());
+                assertThat(allCheckbox.nth(i)).not().isChecked();
             }
+        }
+    }
+
+    @Test
+    void verifyEachCheckbox(){
+        String actual;
+        String expect;
+        for(int i = 0; i < allCheckbox.count(); i++ ){
+            allCheckbox.nth(i).uncheck();
+            allCheckbox.nth(i).check();
+            expect = allCheckboxText.nth(i).innerText();
+            actual = checkedCheckboxText.textContent().replace("\u00A0", "").trim();
+            assertEquals(expect, actual);
+            allCheckbox.nth(i).uncheck();
+        }
+    }
+
+    @Test
+    void verifyMultipleCheckbox(){
+        String actual = "";
+        String expect = "";
+        allCheckbox.nth(0).uncheck();
+
+        for(int i = 0; i < allCheckbox.count(); i++ ){
+            allCheckbox.nth(i).check();
+            expect = expect + " " + allCheckboxText.nth(i).innerText();
+            if(checkedCheckboxText.count() >= 1){
+                for(int j = 0; j < checkedCheckboxText.count(); j++ ){
+                    actual = actual + " " + checkedCheckboxText.nth(j).innerText().replace("\u00A0", "").trim();
+                }
+            }
+            assertEquals(expect, actual);
+            System.out.println(expect + " =" +actual);
+            actual = "";
         }
     }
 }
