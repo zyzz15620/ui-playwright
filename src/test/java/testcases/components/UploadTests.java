490ca79df1cea2f1921655627ae629306b094b6e
phamanhduc <zyzz15620@gmail.com>
2024-11-23 15:43:35 +0700

    [ID-13] Improving UploadTests

--- src/test/java/testcases/components/UploadTests.java
+++ src/test/java/testcases/components/UploadTests.java
@@ -6,33 +6,44 @@ import org.junit.jupiter.api.Test;
 import testcases.BaseTest;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class UploadTests extends BaseTest {
     @Test
-    void verifyUploadFile() {
+    void verifyUploadFile() throws InterruptedException {
         page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/components/upload");
-        String[] inputUploadListLabel = {"image1.png", "image2.png", "JMeter_QuickStart_Guide.pdf"};
+        String input = "image1.png, image2.png, JMeter_QuickStart_Guide.pdf";
+        ArrayList<String> inputUploadListLabel = new ArrayList<>(Arrays.asList(input.split(",\\s*")));
 
         //Method 1 if there are no input node
         String clickToUploadButtonXpath = "//button[span[normalize-space(text())='Click to Upload']]";
         FileChooser fileChooser = page.waitForFileChooser(() -> page.locator(clickToUploadButtonXpath).click());
-        fileChooser.setFiles(Paths.get(inputUploadListLabel[0]));
+        fileChooser.setFiles(Paths.get(inputUploadListLabel.get(0)));
 
         //Method 2 using input node
         String clickToUploadXpath = "//button[span[normalize-space(text())='Click to Upload']]//preceding-sibling::input[@type='file']";
-        page.locator(clickToUploadXpath).setInputFiles(Paths.get(inputUploadListLabel[1]));
-        page.locator(clickToUploadXpath).setInputFiles(Paths.get(inputUploadListLabel[2]));
+        page.locator(clickToUploadXpath).setInputFiles(Paths.get(inputUploadListLabel.get(1)));
+        page.locator(clickToUploadXpath).setInputFiles(Paths.get(inputUploadListLabel.get(2)));
 
-        //Get actual upload list
+        //Get actual upload list and Assert
         String actualUploadListXpath = "//div[contains(concat(' ',normalize-space(@class),' '),' ant-upload-list ')]//span[contains(concat(' ',normalize-space(@class),' '),' ant-upload-list-item-name ')]";
         Locator actualUploadList = page.locator(actualUploadListXpath);
-        String[] actualUploadListLabel = new String[actualUploadList.count()];
-        for (int i = 0; i < actualUploadList.count(); i++) {
-            actualUploadListLabel[i] = actualUploadList.nth(i).innerText();
-        }
+        String[] actualUploadListLabel = actualUploadList.allInnerTexts().toArray(new String[0]);
+        assertArrayEquals(inputUploadListLabel.toArray(), actualUploadListLabel);
 
-        assertArrayEquals(inputUploadListLabel, actualUploadListLabel);
+        //Delete first and Assert again
+        int itemToDelete = 1;
+        String deleteButton = String.format("(%s)[%d]//following-sibling::span//button[@title='Remove file']", actualUploadListXpath, itemToDelete);
+        page.locator(deleteButton).click();
+        inputUploadListLabel.remove(itemToDelete-1);
+
+        //DOM need time to refresh
+        Thread.sleep(1000);
+        String[] actualUploadListLabelAfterDelete = actualUploadList.allInnerTexts().toArray(new String[0]);
+
+        assertArrayEquals(inputUploadListLabel.toArray(), actualUploadListLabelAfterDelete);
     }
 }
